0c79a44516843b13cfb43b58d189d55579429f04
Dereje Gudisa <derejegudisaeme@gmail.com>
2026-03-04 14:01:06 +0300

    structured files

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react';
+import Header from './Header.jsx';
+import Nav from './Nav.jsx';
+import Home from './Home.jsx';
+import Footer from './Footer.jsx';
+import About from './About.jsx';
+import {Routes, Route, useNavigate } from 'react-router-dom';
+
+function App() {
+
+
+  return (
+    <>
+      <div className='App'>
+        <Header />
+        <Nav />
+        <Home />
+        <About />
+        <Footer />
+      </div>
+
+    </>
+  )
+}
+
+export default App
