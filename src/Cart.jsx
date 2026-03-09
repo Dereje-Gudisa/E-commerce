192715b4db4ba571af3d07e70fb8501ef75b0543
Dereje Gudisa <derejegudisaeme@gmail.com>
2026-03-09 23:07:47 +0300

    cart quantity functionality

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -7,52 +7,70 @@ import { RiDeleteBin6Line } from "react-icons/ri";
 
 
 function Cart() {
-  const {cart} = useContext(CartContext);
+  const {cart, updateQuantity} = useContext(CartContext);
+  let pieces = 1;
+
+  function handleIncrease(itemId){
+    updateQuantity(itemId, 1);
+  }
+  function handleDecrease(itemId){
+    updateQuantity(itemId, -1);
+  }
   
   return (
+    
     <div className="cart-page">
       <div className="added-items">
-        <p>Added items</p>
+        <h1>Added items</h1>
+         {cart.length === 0 ?(
+          <p>Your Cart is empty</p>
+        ) :(
+          <>
+
          {cart.map((item) => (
           <div key={item.id} className="cart-item">
-            <img src={item.image} alt="product-img" />
-            <h2>{item.name}</h2>
-            <p>${item.newPrice}</p>
-            <div className="cart-page-buttons">
-              <div className="pcs">
-                <button className="decrease-button">-</button>
-                <p>PCS</p>
-                <button className="increase-button">+</button>
-              </div>
-              <div className="save-delete-button">
-                <button className="save-button"><FaHeart />Save</button>
-                <button className="delete-button"><RiDeleteBin6Line />Delete</button>
+              {JSON.stringify(item.id)}
+            <div className="item-container">
+              <img src={item.image} alt="product-img" />
+              <h2>{item.name}</h2>
+              <p>${item.newPrice}</p>
+              <div className="cart-page-buttons">
+                <div className="pcs">
+                  <button className="decrease-button" onClick={()=>handleDecrease(item.id)}>-</button>
+                  <p>{item.length}PCS</p>
+                  <button className="increase-button" onClick={()=>handleIncrease(item.id)}>+</button>
+                </div>
+                <div className="save-delete-button">
+                  <button className="save-button"><FaHeart />Save</button>
+                  <button className="delete-button"><RiDeleteBin6Line />Delete</button>
+                </div>
               </div>
             </div>
-            
-          </div>
-        ))}
-        {/* {cart.map((item)=>(
-          <Cards key={item.id} product={item} showButton={false} />
-        ))} */}
-      </div>
-      <div className="total-products">
-        <div className="prices-list">
-          <h2 className="product-price">Product  $2000</h2>
-          <h2 className="discount">Discount  $0 </h2>
-          <h2 className="tax">Tax $300</h2>
-          <h2 className="product-price">Service Fee  $0</h2>
-          <h2 className="coupon">Coupon $0</h2>
-          <h1 className="total-price">Total Price $2000</h1>
           
-        </div>
-        <div className="checkout">
-          <button className="checkout-button">Go to checkout</button>
-          <button className="clear-button">Clear</button>
-        </div>
-      </div>
-    </div>
+            <div className="total-products">
+              <div className="prices-list">
+                <h2 className="product-price">Product: ${item.newPrice}</h2>
+                <h2 className="discount">Discount:  $0 </h2>
+                <h2 className="tax">Tax: ${(item.newPrice*0.15)}</h2>
+                <h2 className="product-price">Service Fee: ${item.oldPrice}</h2>
+                <h2 className="coupon">Coupon: $0</h2>
+                <h1 className="total-price">Total Price: $2000</h1>
+              </div>
+              
+              <div className="checkout">
+                <button className="checkout-button">Go to checkout</button>
+                <button className="clear-button">Clear</button>
+              </div>
+            </div>
+          </div>
+    
+        ))}
 
+        </>)}
+      </div>
+      
+
+    </div>
   );
     
 }
--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -5,6 +5,7 @@ export const CartContext = createContext();
 export const CartProvider = ({children}) => {
 
     const [cart, setCart] = useState([]);
+    const [cartPiece, setCartPiece] = useState([]);
     const addToCart = (product) => {
     setCart([...cart, product]);
 
@@ -12,9 +13,16 @@ export const CartProvider = ({children}) => {
         console.log(product.id)
     };
     console.log(cart)
+    const updateQuantity = (id, change) => {
+      setCartPiece(
+        prevCart => prevCart.map(item => 
+                    item.id === id ? {...item, quantity: Math.max(1,(item.quantity || 1)+ change)}
+                    :item )
+      );
+    };
   return (
     <CartContext.Provider value={{ 
-         cart, addToCart
+         cart, addToCart, updateQuantity
     }}> 
         {children}
     </CartContext.Provider>
