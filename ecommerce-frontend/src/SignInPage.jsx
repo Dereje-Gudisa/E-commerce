bd3eb52b70e0621fa36cbc6ae5111a1822ac25c5
https://github.com/Dereje-Gudisa <derejegudisaeme@gmail.com>
2026-06-14 21:24:09 +0300

    api url to render

--- ecommerce-frontend/src/SignInPage.jsx
+++ ecommerce-frontend/src/SignInPage.jsx
@@ -9,8 +9,8 @@ const SignInPage = () => {
   const [password, setPassword] = useState('');
   const [logInMessage, setLogInMessage] = useState('');
 
-  //const API_BASE_URL = 'https://ecommerce-k7mi2hxhe-dereje-gudisas-projects.vercel.app/signInPage';
-  const API_BASE_URL = 'http://localhost:5000';
+  const API_BASE_URL = 'https://e-commerce-e42q.onrender.com';
+  //const API_BASE_URL = 'http://localhost:5000';
 
   const handleSignIn = async (e)=>{
     e.preventDefault();
--- ecommerce-frontend/src/SignUp.jsx
+++ ecommerce-frontend/src/SignUp.jsx
@@ -11,8 +11,8 @@ const SignUp = () => {
   const [phone, setPhone] = useState('');
   const [message, setMessage] = useState('');
 
-  //const API_BASE_URL = 'https://ecommerce-k7mi2hxhe-dereje-gudisas-projects.vercel.app/signInPage';
-  const API_BASE_URL = 'http://localhost:5000';
+  const API_BASE_URL = 'https://e-commerce-e42q.onrender.com';
+  //const API_BASE_URL = 'http://localhost:5000';
 
   const handleSignUpSubmit = async (e) => {
     e.preventDefault();
